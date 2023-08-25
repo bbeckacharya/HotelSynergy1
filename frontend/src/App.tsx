70a0cf398b4696085dbd07758a1ce2a536da3111
Hari Acharya <harryacharya2003@gmail.com>
2023-08-25 06:41:45 +0545

    login UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    console.warn(
+      "DEVELOPER TOOL is for DEVELOPERS only. Please do not paste unknown code or follow others instructions while using this tool, it is only for software developers working in this project. If you are not aware of what you are doing please close this immidately. "
+    );
+  }, []);
   return (
     <>
       <h1>Hello World</h1>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -30,34 +30,59 @@ function Login() {
     <>
       {/* Normal Login Screen */}
       {screen === "Normal" && (
-        <main>
+        <main className="bg-slate-300 flex justify-center items-start h-screen w-screen">
           <form
+            className="flex flex-col rounded-md border-2 border-black bg-white mt-10 p-4 w-1/2  md:w-1/3 xl:w-1/4 gap-1"
             onSubmit={(e) => {
               e.preventDefault();
               handleLogin();
             }}
           >
+            <h1 className="font-bold text-4xl pt-2">Log in</h1>
+            <p className="text-gray-500 pb-2">
+              Please enter details to continue.
+            </p>
+            <label htmlFor="username" className="-mb-1 text-gray-500">
+              Username:
+            </label>
             <input
               type="text"
+              id="username"
               placeholder="Username"
+              autoComplete="Username"
+              className="border-2 p-1 rounded"
               value={user.username}
               onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
+
+            <label htmlFor="password" className="-mb-1 text-gray-500">
+              Password:
+            </label>
             <input
               type="password"
+              id="password"
+              className="border-2 p-1 rounded"
               value={user.password}
               onChange={(e) => setUser({ ...user, password: e.target.value })}
               placeholder="Password"
             />
-            <button type="submit">Log in</button>
-            <button
-              type="reset"
-              onClick={() => {
-                setUser({ username: "", password: "" });
-              }}
-            >
-              Cancle
-            </button>
+            <div className="flex gap-3 md:gap-5 xl:md-10 justify-between mt-2">
+              <button
+                type="reset"
+                className="bg-rose-500 text-white px-5 w-1/2 rounded py-1 hover:bg-rose-800 transition-all"
+                onClick={() => {
+                  setUser({ username: "", password: "" });
+                }}
+              >
+                Cancle
+              </button>
+              <button
+                className="bg-green-500 text-white px-5 w-1/2 rounded py-1 hover:bg-green-800 transition-all"
+                type="submit"
+              >
+                Log in
+              </button>
+            </div>
           </form>
         </main>
       )}
